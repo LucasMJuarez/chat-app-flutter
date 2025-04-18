40ad671911852da5541878033df197bfacc114ac
lucas juarez <lujuarez@emeal.nttdata.com>
2025-04-18 19:27:46 -0300

    replicate meesage bubble for her

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
+            ),
+            child: Text(
+              "hola soy ellia",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        SizedBox(height: 5),
+
+        //todo image
+      ],
+    );
+  }
+}
